403e079354b21569136dd97fd1485de0b76701fa
Jared <jagiangrasso@gmail.com>
2020-12-07 19:20:54 -0500

    begin adding login form

--- public/GameClient.js
+++ public/GameClient.js
@@ -11,13 +11,11 @@ class Game {
   addPlayer(newPlayer, playerCount) {
     this._playerCount = playerCount;
     this._players[newPlayer.pid] = newPlayer;
-    console.log(this._playerCount);
   }
 
   removePlayer(pid) {
     this._playerCount -= 1;
     const { [pid]: _, ...restPlayers } = this._players;
     this._players = restPlayers;
-    console.log(this._playerCount);
   }
 }
--- public/index.js
+++ public/index.js
@@ -2,25 +2,22 @@ const socket = io();
 
 const game = new Game();
 
-// const handleFormSubmission = () => {
-//   const logSubmit = (e) => {
-//     e.preventDefault();
-//     socket.emit('chat message', document.querySelector('#m').value);
-//     document.querySelector('#m').value;
-//     return false;
-//   };
-
-//   const form = document.querySelector('form');
-//   form.addEventListener('submit', logSubmit);
-// }
-
 const setPlayerCount = () => {
   const { playerCount } = game;
   document.querySelector('#playerCount').textContent = playerCount;
 };
 
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(document.forms['login-form']);
+  const name = formData.get('name');
+  const color = formData.get('color');
+  // send name, color to server
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  // handleFormSubmission();
+  const form = document.forms['login-form'];
+  form.addEventListener('submit', handleFormSubmit, false);
 
   socket.on('add player', ({ newPlayer, playerCount }) => {
     game.addPlayer(newPlayer, playerCount);
